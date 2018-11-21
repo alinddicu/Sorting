bf49726ca9765825eaa3abf7e8fdc3afec2cd38a
Alin Dicu <alinddicu@yahoo.com>
2018-11-21 18:41:02 +0100

    NoDoublesGenerator ok

---
+++ Sorting/Sorting.Tests/GeneratorTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NFluent;
+
+namespace Sorting.Tests
+{
+	[TestClass]
+	public class GeneratorTest
+	{
+		[TestMethod]
+		public void GivenFrom1To100WhenExecuteThanSequenceIsCorrect()
+		{
+			var array = new NoDoublesGenerator().Execute();
+
+			Check.That(array.Sum(i => i)).IsEqualTo(5050);
+		}
+	}
+}
---
+++ Sorting/Sorting/NoDoublesGenerator.cs
@@ -0,0 +1,17 @@
+﻿namespace Sorting
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using Medallion;
+
+	public class NoDoublesGenerator
+    {
+		public IEnumerable<int> Execute()
+		{
+			//var rnd = new Random();
+			//return Enumerable.Range(1, 100).OrderBy(x => rnd.Next());
+			return Enumerable.Range(1, 100).Shuffled(new Random()).ToArray();
+		}
+    }
+}
