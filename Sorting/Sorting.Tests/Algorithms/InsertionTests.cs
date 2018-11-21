fc905512c357a642220052e6beda98f7a6ea7c06
Alin Dicu <alinddicu@yahoo.com>
2018-11-21 20:59:55 +0100

    insertion ok

---
+++ Sorting/Sorting.Tests/Algorithms/InsertionTests.cs
@@ -0,0 +1,24 @@
+﻿namespace Sorting.Tests.Algorithms
+{
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+	using NFluent;
+	using Sorting.Algorithms;
+	using System.Linq;
+
+	[TestClass]
+	public class InsertionTests
+	{
+
+		[TestMethod]
+		public void GivenInsertionWhenExecuteThenResultIsOrdered()
+		{
+			var topLimit = 5;
+			var expected = Enumerable.Range(1, topLimit).ToArray();
+			var testArray = new NoDoublesGenerator().Execute(topLimit);
+			//var testArray = new[] { 5, 4, 3, 2, 1 };
+			var result = new Insertion().Execute(testArray).ToArray();
+
+			Check.That(result).IsEqualTo(expected);
+		}
+	}
+}
---
+++ Sorting/Sorting/Algorithms/Insertion.cs
@@ -0,0 +1,52 @@
+﻿namespace Sorting.Algorithms
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	public class Insertion
+	{
+		public IEnumerable<int> Execute(IEnumerable<int> input)
+		{
+			var unsortedList = Copy(input).ToList();
+			var sortedList = new List<int>();
+			foreach (var unsortedItem in unsortedList)
+			{
+				var inserted = false;
+				for (int i = sortedList.Count - 1; !inserted && i >= 0; i--)
+				{
+					var sortedItem = sortedList[i];
+					int? sortedItemMinus1 = null;
+					if (i != 0 && sortedList.Count >= 2)
+					{
+						sortedItemMinus1 = sortedList[i - 1];
+					}
+
+					if (unsortedItem < sortedItem && (!sortedItemMinus1.HasValue || unsortedItem > sortedItemMinus1))
+					{
+						sortedList.Insert(i, unsortedItem);
+						inserted = true;
+					}
+				}
+
+				if (!inserted)
+				{
+					sortedList.Add(unsortedItem);
+				}
+			}
+
+			return sortedList;
+		}
+
+		private IEnumerable<int> Copy(IEnumerable<int> input)
+		{
+			foreach (var item in input)
+			{
+				yield return item;
+			}
+		}
+
+	}
+}
