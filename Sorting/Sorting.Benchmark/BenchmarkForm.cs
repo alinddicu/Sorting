3a2ca8025b3e7d4cea990d6d55cdf4b90afb7d43
Alin Dicu <alinddicu@yahoo.com>
2018-11-23 10:10:19 +0100

    minor refactoc into InitBenchWorkers

--- Sorting/Sorting.Benchmark/BenchmarkForm.cs
+++ Sorting/Sorting.Benchmark/BenchmarkForm.cs
@@ -1,39 +1,34 @@
 ﻿namespace Sort.Benchmark
 {
-	using System.Drawing;
-	using System.Linq;
-	using System.Windows.Forms;
+    using System.Linq;
+    using System.Windows.Forms;
 
-	using Sorting;
-	using Sorting.Algorithms;
+    using Sorting;
+    using Sorting.Algorithms;
 
-	public partial class BenchmarkForm : Form
-	{
-		public BenchmarkForm()
-		{
-			InitializeComponent();
-            var basePanel = new FlowLayoutPanel
-            {
-                Dock = DockStyle.Fill
-            };
+    public partial class BenchmarkForm : Form
+    {
+        public BenchmarkForm()
+        {
+            InitializeComponent();
+            InitBenchWorkers();
+        }
+
+        private void InitBenchWorkers()
+        {
+            var sorts = new SortBase [] { new BubbleSort(), new InsertionSort(), new SelectionSort() };
+
+            var basePanel = new FlowLayoutPanel { Dock = DockStyle.Fill };
             Controls.Add(basePanel);
-
             var unsortedList = new UniqueElementsGenerator().Execute(100).ToArray();
 
-			var bubblePanel = new BenchPanel(new BubbleSort(), unsortedList);
-            basePanel.Controls.Add(bubblePanel);
-            var bubbleBwWorker = new BenchBackgroundWorker(bubblePanel);
-			bubbleBwWorker.RunWorkerAsync();
-
-			var insertionPanel = new BenchPanel(new InsertionSort(), unsortedList);
-            basePanel.Controls.Add(insertionPanel);
-			var insertionBwWorker = new BenchBackgroundWorker(insertionPanel);
-			insertionBwWorker.RunWorkerAsync();
-
-			var selectionPanel = new BenchPanel(new InsertionSort(), unsortedList);
-            basePanel.Controls.Add(selectionPanel);
-			var selectionBwWorker = new BenchBackgroundWorker(selectionPanel);
-			selectionBwWorker.RunWorkerAsync();
-		}
-	}
+            foreach (var sort in sorts)
+            {
+                var bubblePanel = new BenchPanel(sort, unsortedList);
+                basePanel.Controls.Add(bubblePanel);
+                var bubbleBwWorker = new BenchBackgroundWorker(bubblePanel);
+                bubbleBwWorker.RunWorkerAsync();
+            }
+        }
+    }
 }
