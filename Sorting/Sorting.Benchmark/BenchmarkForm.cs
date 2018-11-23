7c2d2bb603046011a0f44a1530a10b39cf34bf0e
Alin Dicu <alinddicu@yahoo.com>
2018-11-23 22:51:11 +0100

    minor refactoring

--- Sorting/Sorting.Benchmark/BenchmarkForm.cs
+++ Sorting/Sorting.Benchmark/BenchmarkForm.cs
@@ -1,52 +1,48 @@
 ﻿namespace Sort.Benchmark
 {
-    using System.Linq;
-    using System.Windows.Forms;
+	using System.Linq;
+	using System.Windows.Forms;
 
-    using Sorting;
-    using System.Reflection;
-    using System.Collections.Generic;
+	using Sorting;
+	using System.Reflection;
+	using System.Collections.Generic;
 
-    public partial class BenchmarkForm : Form
-    {
-        private readonly List<string> _sortRanking = new List<string>();
-        private readonly int _maxSortValue;
+	public partial class BenchmarkForm : Form
+	{
+		private readonly List<string> _sortRanking = new List<string>();
+		private readonly int _maxSortValue;
 
-        public BenchmarkForm(int maxSortValue)
-        {
-            _maxSortValue = maxSortValue;
-            InitializeComponent();
-            InitForm();
-            InitBenchWorkers();
-        }
+		public BenchmarkForm(int maxSortValue)
+		{
+			_maxSortValue = maxSortValue;
+			InitializeComponent();
+			InitForm();
+			InitBenchWorkers();
+		}
 
-        private void InitForm()
-        {
-            Width = 445;
-            Height = 480;
-            CenterToScreen();
-        }
+		private void InitForm()
+		{
+			Width = 445;
+			Height = 480;
+			CenterToScreen();
+		}
 
-        private void InitBenchWorkers()
-        {
-            var basePanel = new FlowLayoutPanel
-            {
-                Width = Width,
-                Height = Height,
-                Dock = DockStyle.Fill
-            };
-            Controls.Add(basePanel);
-            var unsortedList = new UniqueElementsGenerator().Execute(_maxSortValue).ToArray();
-			
+		private void InitBenchWorkers()
+		{
+			var basePanel = new FlowLayoutPanel { Width = Width, Height = Height, Dock = DockStyle.Fill };
+
+			var unsortedList = new UniqueElementsGenerator().Execute(_maxSortValue);
 			var benchPanels = Assembly.GetAssembly(typeof(SortBase))
 				.GetHeirsOf<SortBase>()
 				.Select(sort => new BenchPanel(sort, unsortedList, _maxSortValue));
 			foreach (var benchPanel in benchPanels)
-            {
-	            basePanel.Controls.Add(benchPanel);
-	            var bubbleBwWorker = new BenchBackgroundWorker(benchPanel, _sortRanking);
-	            bubbleBwWorker.RunWorkerAsync();
-            }
-        }
-    }
+			{
+				basePanel.Controls.Add(benchPanel);
+				var bubbleBwWorker = new BenchBackgroundWorker(benchPanel, _sortRanking);
+				bubbleBwWorker.RunWorkerAsync();
+			}
+
+			Controls.Add(basePanel);
+		}
+	}
 }
