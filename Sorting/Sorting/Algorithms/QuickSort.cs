dede67ccb2b3db2803ed4534a0db63d7679177d0
Alin Dicu <alinddicu@yahoo.com>
2018-11-23 23:25:32 +0100

    QuickSort implemented

--- Sorting/Sorting/Algorithms/QuickSort.cs
+++ Sorting/Sorting/Algorithms/QuickSort.cs
@@ -1,80 +1,55 @@
 ﻿namespace Sorting.Algorithms
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
+	using System.Collections.Generic;
+	using System.Linq;
 
-    public class QuickSort// : SortBase
-    {
-        //public override IEnumerable<int> Execute(IEnumerable<int> unsortedList)
-        //{
-        //    var copy = Copy(unsortedList).ToArray();
+	public class QuickSort : SortBase
+	{
+		private int[] _copy;
 
-        //    //Quicksort(copy, 0, copy.Length - 1);
+		// https://www.geeksforgeeks.org/quick-sort/
+		public override IEnumerable<int> Execute(IEnumerable<int> unsortedList)
+		{
+			_copy = Copy(unsortedList).ToArray();
 
-        //    return copy;
-        //}
+			Quicksort(_copy, 0, _copy.Length - 1);
 
-        /*
-        algorithm quicksort(A, lo, hi) is
-            if lo < hi then
-                p := partition(A, lo, hi)
-                quicksort(A, lo, p - 1 )
-                quicksort(A, p + 1, hi)         
-        */
-        private void Quicksort(int[] A, int lo, int hi)
-        {
-            if (lo < hi)
-            {
-                var pivot = Partition(A, lo, hi);
-                Quicksort(A, lo, pivot - 1);
-                Quicksort(A, pivot + 1, hi);
-            }
-        }
+			return _copy;
+		}
 
-        /*
-        algorithm partition(A, lo, hi) is
-            pivot := A[hi]
-            i := lo - 1  
-            for j := lo to hi - 1 do
-                if A[j] < pivot then
-                    if i != j then
-                        i := i + 1
-                        swap A[i] with A[j]
-           
-            i := i + 1
-            swap A[i] with A[hi]
-            return i
-         */
-        private int Partition(int[] A, int lo, int hi)
-        {
-            var pivot = A[hi];
-            var i = lo - 1;
-            for (int j = lo; j < hi - 1;)
-            {
-                if (A[j] < pivot)
-                {
-                    if (i != j)
-                    {
-                        i = i + 1;
-                        var temp = A[j];
-                        A[j] = A[i];
-                        A[i] = temp;
-                    }
-                }
+		private void Quicksort(IList<int> array, int lo, int hi)
+		{
+			if (lo < hi)
+			{
+				var pivot = Partition(array, lo, hi);
+				Quicksort(array, lo, pivot - 1);
+				Quicksort(array, pivot + 1, hi);
+			}
+		}
 
-                i = i + 1;
-                if (i >= A.Length)
-                {
-                    break;
-                }
+		private int Partition(IList<int> array, int lo, int hi)
+		{
+			var pivot = array[hi];
+			var i = lo - 1;
+			for (var j = lo; j <= hi - 1; j++)
+			{
+				if (array[j] <= pivot)
+				{
+					i++;
+					Swap(array, i, j);
+				}
+			}
 
-                var temp2 = A[j];
-                A[j] = A[i];
-                A[i] = temp2;
-            }
+			Swap(array, i + 1, hi);
+			AddToIntermediateSorts(_copy);
+			return i + 1;
+		}
 
-            return i;
-        }
-    }
+		private void Swap(IList<int> array, int i, int j)
+		{
+			var temp = array[j];
+			array[j] = array[i];
+			array[i] = temp;
+		}
+	}
 }
