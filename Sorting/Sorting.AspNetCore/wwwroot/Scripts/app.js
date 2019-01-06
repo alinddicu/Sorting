71d93dc59f346136218d218a589d73c2491916bd
Alin Dicu <alinddicu@yahoo.com>
2019-01-06 23:19:30 +0100

    BubbleSort works!!!!

--- Sorting/Sorting.AspNetCore/wwwroot/Scripts/app.js
+++ Sorting/Sorting.AspNetCore/wwwroot/Scripts/app.js
@@ -1,6 +1,8 @@
-function sayHello() {
-    const compiler = document.getElementById("compiler").value;
-    const framework = document.getElementById("framework").value;
-    return `Hello from ${compiler} and ${framework}!`;
+/// <reference path="./Algorithms/BubbleSort.ts"/>
+function testSort() {
+    let sort = new Algorithms.BubbleSort();
+    let result = sort.execute([3, 2, 1]);
+    console.log("result: " + result);
+    return JSON.stringify(result); //.trim("]").trim("[");
 }
 //# sourceMappingURL=app.js.map
