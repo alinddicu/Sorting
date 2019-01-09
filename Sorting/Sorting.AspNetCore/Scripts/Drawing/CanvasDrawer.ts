3be572a9d29a7775b1db8f69254bb342e566ca23
Alin Dicu <alinddicu@yahoo.com>
2019-01-09 21:22:03 +0100

    setInterval works

--- Sorting/Sorting.AspNetCore/Scripts/Drawing/CanvasDrawer.ts
+++ Sorting/Sorting.AspNetCore/Scripts/Drawing/CanvasDrawer.ts
@@ -10,8 +10,8 @@ namespace Drawing {
 		lineTo(x:number, y:number): void;
 		stroke(): void;
 		fillStyle: string;
-		clearRect(x: number, y: number, width: number, height: number): void
-;
+		clearRect(x: number, y: number, width: number, height: number): void;
+		strokeStyle: string;
 	}
 
 	interface ICanvas {
@@ -29,26 +29,29 @@ namespace Drawing {
 		public Draw(canvas: ICanvas): void {
 			const intermediateSorts = this.sorting.getIntermediateSorts();
 			var ctx = canvas.getContext("2d");
-			ctx.fillStyle = "#FF0000";
+			ctx.strokeStyle = "#FF0000";
 
-			for (let i = 0; i < intermediateSorts.length; i++) {
+			let i = 0;
 
-				setTimeout(() => {
-					ctx.clearRect(0, 0, canvas.width, canvas.height);
-					ctx.beginPath();
+			let refreshId = setInterval(() => {
+				ctx.clearRect(0, 0, canvas.width, canvas.height);
+				ctx.beginPath();
 
-					let intermediateSort = intermediateSorts[i];
-					//console.log("intermediateSort: " + intermediateSort);
-					for (let x = 0; x < intermediateSort.length; x++) {
-						const y = intermediateSort[x];
-						ctx.moveTo(x*10, 0);
-						//console.log(`ctx.moveTo(${x}, 0)`);
-						ctx.lineTo(x*10, y*10);
-						//console.log(`ctx.lineTo(${x}, ${y})`);
-						ctx.stroke();
-					}					
-				}, 1000);
-			}
+				let intermediateSort = intermediateSorts[i++];
+				//console.log("intermediateSort: " + intermediateSort);
+				for (let x = 0; x < intermediateSort.length; x++) {
+					const y = intermediateSort[x];
+					ctx.moveTo(x*10, 0);
+					//console.log(`ctx.moveTo(${x}, 0)`);
+					ctx.lineTo(x*10, y*10);
+					//console.log(`ctx.lineTo(${x}, ${y})`);
+					ctx.stroke();
+				}
+
+				if (i >= intermediateSorts.length) {
+					clearInterval(refreshId);
+				}
+			}, 200);
 		}
 	}
 }
