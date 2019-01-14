f2a9dc83a0a091fa0e0b8a9198456278b8aed1f0
Alin Dicu <alinddicu@yahoo.com>
2019-01-14 18:41:45 +0100

    casting in TypeScript :(

--- Sorting/Sorting.WebPure/scripts/app.ts
+++ Sorting/Sorting.WebPure/scripts/app.ts
@@ -1,13 +1,29 @@
 ﻿function sort() {
+	function valueOrDefault<T>(elementId: string, defaultValue: any): T {
+		const inputField = document.getElementById(elementId) as HTMLFormElement;
+		let value;
+		if (typeof defaultValue == "number") {
+			value = inputField.valueAsNumber;
+		}
+		else if (typeof defaultValue == "string") {
+			value = inputField.valueAsString;
+		} else {
+			value = inputField.valueAsDate;
+		}
+
+		return value ? value : defaultValue;
+	}
+
 	const drawParams = {
-		step: (document.getElementById("step") as HTMLFormElement).valueAsNumber as number,
-		delay: (document.getElementById("delay") as HTMLFormElement).valueAsNumber as number,
-		penColor: (document.getElementById("penColor") as HTMLFormElement).value as string,
-		backgroundColor: (document.getElementById("backgroundColor") as HTMLFormElement).value as string,
-		fontSize: (document.getElementById("fontSize") as HTMLFormElement).valueAsNumber as number
+		step: valueOrDefault<number>("step", 10),
+		delay: valueOrDefault<number>("delay", 50),
+		penColor: valueOrDefault<string>("penColor", "lime"),
+		backgroundColor: valueOrDefault<string>("backgroundColor", "black"),
+		fontSize: valueOrDefault<number>("fontSize", 16)
 	};
-	const arraySize = (document.getElementById("arraySize") as HTMLFormElement).valueAsNumber as number;
-	const startValue = (document.getElementById("startValue") as HTMLFormElement).valueAsNumber as number;
+
+	const arraySize = valueOrDefault<number>("arraySize", 34);
+	const startValue = valueOrDefault<number>("startValue", 1);
 	const arrayToSort = new Tools.RandomArrayGenerator().Generate(arraySize, startValue);
 	new Drawing.MultiCanvasDrawer(document, drawParams).draw(arrayToSort);
 }
